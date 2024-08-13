3bb724d281824a159b3c86211a224c5b52f4dd48
Darek Podczasik <boostertech@mail.com>
2024-08-13 12:51:51 +0100

    useSearchParams() added testing!

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import styles from "../components/Map.module.css";
+import { useSearchParams } from "react-router-dom";
 
 const Map = () => {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h2>
+        <p>Position:</p>
+        <p>lat: {lat}</p>
+        <p>lng: {lng}</p>
+      </h2>
+      <button onClick={() => setSearchParams({ lat: 19, lng: 50 })} >click</button>
+    </div>
+  );
 };
 
 export default Map;
