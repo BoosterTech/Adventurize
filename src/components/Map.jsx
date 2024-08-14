e6df8520d1409320a4d02719f415f2537c4e1b4f
Darek Podczasik <boostertech@mail.com>
2024-08-14 10:08:18 +0100

    open <Form/> by clicking on <Map/> functionality added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import styles from "../components/Map.module.css";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Map = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-
+  const navigate = useNavigate();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
   return (
-    <div className={styles.mapContainer}>
+    <div
+      className={styles.mapContainer}
+      onClick={() => {
+        navigate("form");
+      }}
+    >
       <h1>Map</h1>
       <h2>
         <p>Position:</p>
         <p>lat: {lat}</p>
         <p>lng: {lng}</p>
       </h2>
-      <button onClick={() => setSearchParams({ lat: 19, lng: 50 })} >click</button>
+      <button onClick={() => setSearchParams({ lat: 19, lng: 50 })}>
+        click
+      </button>
     </div>
   );
 };
