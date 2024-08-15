f91dc8735d98cb38886a363db515c7984b25ea50
Darek Podczasik <boostertech@mail.com>
2024-08-15 11:17:44 +0100

    footer text updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
       <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
-          &copy; Copyright{new Date().getFullYear()} by WorldWize Inc.
+          &copy; Copyright{new Date().getFullYear()} by Adventurize Inc.
         </p>
       </footer>
     </div>
