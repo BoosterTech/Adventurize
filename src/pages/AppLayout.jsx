065121ab57931900e02851cdbd8476c5c1fc502f
Darek Podczasik <boostertech@mail.com>
2024-08-17 21:30:06 +0100

    <User/> added to <AppLayout/>

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Sidebar from "../components/Sidebar";
 import styles from "../pages/AppLayout.module.css";
 import Map from "../components/Map";
+import User from "../components/User";
 
 const AppLayout = () => {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 };
