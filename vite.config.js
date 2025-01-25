1e0f3e98b51a43536a9d86a22d3140d4e5e33042
Dariusz Podczasik <boostertech@mail.com>
2025-01-25 20:38:21 +0000

    deployment updates

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), eslint()],
-  build: { chunkSizeWarningLimit: 1000, outDir: "build" },
-  // base: "/Adventurize",
+export default defineConfig(({ command }) => {
+  const isProduction = command === "build"; // check if we're in production mode
+  return {
+    plugins: [react(), eslint()],
+    build: { chunkSizeWarningLimit: 1000, outDir: "build" },
+    base: isProduction ? "/Adventurize/" : "/", // Use "/" for development and "/Adventurize/" for production
+  };
 });
