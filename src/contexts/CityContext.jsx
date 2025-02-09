42cbe1203638a25fb78c759815266639a8552388
Dariusz Podczasik <boostertech@mail.com>
2025-02-09 19:05:32 +0000

    test: deployment changes (move cities.json to data/public folder)

--- src/contexts/CityContext.jsx
+++ src/contexts/CityContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 
-const BASE_URL = "http://localhost:9000";
+const BASE_URL = "/data/cities/json";
 
 const CitiesContext = createContext();
 
