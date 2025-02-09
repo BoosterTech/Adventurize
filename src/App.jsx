26d68b674bafe0b0048f5904a7c0657a34e7fa25
Dariusz Podczasik <boostertech@mail.com>
2025-02-09 18:29:58 +0000

    test: deployment changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,19 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import { CitiesProvider } from "./contexts/CityContext";
 import { AuthProvider } from "./contexts/AuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/Adventurize">
           <Routes>
-            <Route path="/Adventurize" element={<Homepage />} />
+            <Route path="/" element={<Homepage />} />
             <Route path="/product" element={<Product />} />
             <Route path="/pricing" element={<Pricing />} />
             <Route path="/login" element={<Login />} />
@@ -33,10 +35,10 @@ function App() {
               }
             >
               <Route index element={<Navigate replace to="cities" />} />
-              <Route path="/cities" element={<CityList />} />
-              <Route path="/cities/:id" element={<City />} />
-              <Route path="/countries" element={<CountryList />} />
-              <Route path="/form" element={<Form />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
             </Route>
             <Route path="*" element={<PageNotFound />} />
           </Routes>
